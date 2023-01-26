f90c2585b4f1e0451caee0b7a87d0a33703d9384
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-01-26 17:38:08 +0200

    third-commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,60 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const gallery = document.querySelector('.gallery');
-const searchForm = document.querySelector('.search-form')
+const searchForm = document.querySelector('.search-form');
+const loadMoreBtn = document.querySelector('.load-more');
 
-searchForm.addEventListener('submit', onSearch)
+let page = 1;
+let searchQuery = '';
 
-function onSearch(e){
-    e.preventDefault()
-    const searchQuery = e.currentTarget.elements.searchQuery.value;
-    console.log(searchQuery)
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onClick);
 
-    fetchPixabayApi(searchQuery)
-  .then(data => createCardMarkup(data.hits))
-  .catch(error => console.log(error));
+function onSearch(e) {
+  e.preventDefault();
+ searchQuery = e.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery);
+
+  fetchPixabayApi(searchQuery)
+    .then(data => {
+      createCardMarkup(data.hits);
+      console.log(data);
+      loadMoreBtn.hidden = false;
+    })
+    .catch(error => console.log(error));
 }
 
-async function fetchPixabayApi() {
-  // const BASE_URL = 'https://pixabay.com/api/';
-  // const KEY = '33084667-f5fdd61fd2318acf30785d2ee';
+function onClick() {
+  page += 1;
+  fetchPixabayApi(searchQuery, page).then(data => {
+    createCardMarkup(data.hits);
+    console.log(data)
+    // if (data.hits.length === data.totalHits) {
+    //   loadMoreBtn.hidden = true;
+    //   Notiflix.Notify.info('We\'re sorry, but you\'ve reached the end of search results.');
+    // }
+  });
+}
+
+
+
+async function fetchPixabayApi(query, page = 1) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const KEY = '33084667-f5fdd61fd2318acf30785d2ee';
+
+  const searchParams = new URLSearchParams({
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
 
   const response = await axios.get(
-    'https://pixabay.com/api/?key=33084667-f5fdd61fd2318acf30785d2ee&q=cat&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40'
+    `${BASE_URL}?key=${KEY}&page=${page}&per_page=100&q=${query}&${searchParams}`
   );
+  console.log(response)
   return response.data;
 }
-
-
 
 function createCardMarkup(arr) {
   const markup = arr
@@ -40,7 +69,7 @@ function createCardMarkup(arr) {
         downloads,
       }) => `
       <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width = "300px" height = "200px"/>
   <div class="info">
     <p class="info-item">
       <b>Likes ${likes}</b>
@@ -59,5 +88,5 @@ function createCardMarkup(arr) {
     )
     .join('');
 
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
